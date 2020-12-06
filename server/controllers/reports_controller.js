0a6b143c965592b68fb15b2964cd2b8c6c2e4f5c
dyppaul <fairbattle@hotmail.com>
2020-12-06 14:49:15 -0800

    Added get, create, delete endpoints for reports controller

--- server/controllers/reports_controller.js
+++ server/controllers/reports_controller.js
@@ -20,3 +20,28 @@ exports.get_all_reports = async (req, res) => {
     });
   }
 }
+
+exports.get_report_by_id = async (req, res) => {
+  const report_id = req.params.report_id;
+  query = {
+    text: `select *
+            from reports
+            where report_id = $1`,
+    values: [report_id]
+  }
+
+  try {
+    const query_result = await connection.query(query);
+    res.send({
+      success: true,
+      content: query_result.rows[0]
+    })
+  } catch(err) {
+    console.error(err);
+    res.send({
+      success: false,
+      content: err.detail
+    });
+  }
+}
+
