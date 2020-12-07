f29412af7e60d6a434de1b945823c9bb6f91b589
Kevin0115 <kevin.choi0115@gmail.com>
2020-12-06 18:03:47 -0800

    added frontend stuff for kenneth

--- server/sdw.sql
+++ server/sdw.sql
@@ -10,4 +10,15 @@ create table reports (
   evidence bytea
 );
 
+create table reporters (
+  reporter_id varchar(10) not null primary key,
+  name varchar(30) not null,
+  phone varchar(10) not null,
+  address varchar(30) not null,
+  email varchar(30) not null,
+  rank integer not null,
+);
+
 insert into reports values (0123456789, 'submitted', 9876543210, '2020-11-30 12:00:00', 1000, 'Vancouver, BC', 'Doon', 'Going to a rave');
+
+insert into reporters values (9876543210, 'Doon', '6044206969', 'Langley, BC', 'email@email.com', 100);
