944b4217693c858f430ed1453df5b0650b8f293b
garyhwu <garyh1998@gmail.com>
2020-12-06 16:16:05 -0800

    post and delete reporter functions

--- server/controllers/reporters_controller.js
+++ server/controllers/reporters_controller.js
@@ -44,3 +44,56 @@ exports.get_reporter_by_id = async (req, res) => {
     });
   }
 }
+
+exports.create_reporter = async (req, res) => {
+  const reporter_id = req.body.reporter_id;
+  const name = req.body.name;
+  const phone = req.body.phone;
+  const address = req.body.address;
+  const email = req.body.email;
+  const rank = 100;
+
+  query = {
+    text: `insert into reporters
+            values ($1,$2,$3,$4,$5,$6)`,
+    values: [reporter_id, name, phone, address, email, rank]
+  }
+
+  try {
+    const query_result = await connection.query(query);
+    console.log(query_result);
+    res.send({
+      success: true,
+      content: reporter_id
+    });
+  } catch(err) {
+    console.error(err);
+    res.send({
+      success: false,
+      content: err.detail
+    });
+  }
+}
+ 
+exports.delete_reporter = async (req, res) => {
+  const report_id = req.params.reporter_id;
+  query = {
+    text: `delete from reporters
+            where reporter_id = $1`,
+    values: [reporter_id]
+  }
+
+  try {
+    const query_result = await connection.query(query);
+    res.send({
+      success: true,
+      content: report_id
+    })
+  } catch(err) {
+    console.error(err);
+    res.send({
+      success: false,
+      content: err.detail
+    });
+  }
+}
