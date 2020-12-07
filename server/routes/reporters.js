6050ca75694bbe7194d6b55c9bf2082ac9d43507
dyppaul <fairbattle@hotmail.com>
2020-12-06 16:52:46 -0800

    Updated route parameter to include reporter_id in reporter.js

--- server/routes/reporters.js
+++ server/routes/reporters.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', reporters_controller.get_all_reporters);
+var reports_controller = require('../controllers/reports_controller');
 
-router.get('/', reporters_controller.get_reporter_by_id);
+router.get('/', reports_controller.get_all_reports);
 
-router.post('/', reporters_controller.create_reporter);
+router.get('/:report_id', reports_controller.get_report_by_id);
 
-router.delete('/', reporters_controller.delete_reporter);
+router.post('/', reports_controller.create_report);
+
+router.delete('/:report_id', reports_controller.delete_report);
+
+router.put('/:report_id/status', reports_controller.update_report_status);
+
+router.put('/:report_id/details', reports_controller.update_report_details);
 
 module.exports = router;
