be1a485354c892d11d62b591703c67cdd2b88489
edmundohh <1edmundoh@gmail.com>
2020-12-06 17:29:43 -0800

    edit route import for reporters in index.js

--- server/controllers/reporters_controller.js
+++ server/controllers/reporters_controller.js
@@ -2,24 +2,24 @@ const connection = require('../config/db_config');
 const moment = require('moment');
 
 exports.get_all_reporters = async (req, res) => {
-    query = {
-      text: `select * from reporters`
-    }
-  
-    try {
-      const query_result = await connection.query(query);
-      res.send({
-        success: true,
-        content: query_result.rows
-      });
-    } catch(err) {
-      console.error(err);
-      res.send({
-        success: false,
-        content: err.detail
-      });
-    }
+  query = {
+    text: `select * from reporters`
   }
+
+  try {
+    const query_result = await connection.query(query);
+    res.send({
+      success: true,
+      content: query_result.rows
+    });
+  } catch(err) {
+    console.error(err);
+    res.send({
+      success: false,
+      content: err.detail
+    });
+  }
+}
 
 exports.get_reporter_by_id = async (req, res) => {
   const reporter_id = req.params.reporter_id;
@@ -76,7 +76,7 @@ exports.create_reporter = async (req, res) => {
 }
  
 exports.delete_reporter = async (req, res) => {
-  const report_id = req.params.reporter_id;
+  const reporter_id = req.params.reporter_id;
   query = {
     text: `delete from reporters
             where reporter_id = $1`,
@@ -87,7 +87,7 @@ exports.delete_reporter = async (req, res) => {
     const query_result = await connection.query(query);
     res.send({
       success: true,
-      content: report_id
+      content: reporter_id
     })
   } catch(err) {
     console.error(err);
